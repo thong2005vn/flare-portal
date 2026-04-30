8d46cd3728b21d8ebffce8573d3a0a6da33077ce
thong2005vn <30981827+thong2005vn@users.noreply.github.com>
2026-04-30 18:40:42 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FlarePortal from './App.jsx' // Import component chính của bạn
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FlarePortal />
+  </React.StrictMode>,
+)
